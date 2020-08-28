7cd65537fc084343bea7ace1d083ae8e9f484aab
miguel_casas <miguel.casas@atmira.com>
2020-08-28 19:25:27 +0200

    guard: usar un guard para entrar en una página solo cuando se cumple una condición.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
+
 import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
@@ -9,7 +11,14 @@ import { ProtegidaComponent } from './components/protegida/protegida.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'precios', component: PreciosComponent },
-  { path: 'protegida', component: ProtegidaComponent },
+  // añadir el guard a 'protegida' para que solo se permita entrar en 'protegida' si se cumple
+  // el guard.
+  { path: 'protegida',
+    component: ProtegidaComponent,
+    // canActivate es una propiedad que es un array con todos los guard que se deben cumplir
+    // para entrar en la página.
+    canActivate: [ AuthGuard ]
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
